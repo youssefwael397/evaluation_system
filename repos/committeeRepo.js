ec872304cfef9489eb86cefe3b5806a6f1e00976
mariam887 <68079005+mariam887@users.noreply.github.com>
2022-03-17 01:18:18 +0200

    Update committeeRepo.js

--- repos/committeeRepo.js
+++ repos/committeeRepo.js
@@ -1,59 +1,5 @@
 const { Committee } = require('../models/index')
 
-const committeesList = [
-    {
-        committee_name: 'WEB',
-    },
-    {
-        committee_name: 'ANDROID',
-    },
-    {
-        committee_name: 'HRM',
-    },
-    {
-        committee_name: 'HRD',
-    },
-    {
-        committee_name: 'ACADEMY',
-    },
-    {
-        committee_name: 'BD',
-    },
-    {
-        committee_name: 'E4ME',
-    },
-    {
-        committee_name: 'EXTRA',
-    },
-    {
-        committee_name: 'IR',
-    },
-    {
-        committee_name: 'LOGISTICS',
-    },
-    {
-        committee_name: 'MULTIMEDIA',
-    },
-    {
-        committee_name: 'Magazine_Designs',
-    },
-    {
-        committee_name: 'Magazine_Editing',
-    },
-    {
-        committee_name: 'OC',
-    },
-    {
-        committee_name: 'OFFLINE_MARKETING',
-    },
-    {
-        committee_name: 'SOCIAL_MEDIA',
-    },
-    {
-        committee_name: 'DATA SCINCE',
-    },
-]
-
 
 const getAllCommittees = async () => {
     const committees = await Committee.findAll({ attributes: ['committee_id', 'committee_name'] })
