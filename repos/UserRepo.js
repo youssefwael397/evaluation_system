58b6cf69f73d8469731c75cb8e283ce808377836
Youssefwael397 <youssefwael397@gmail.com>
2022-04-12 17:06:25 +0200

    get board handle errors in query table names

--- repos/UserRepo.js
+++ repos/UserRepo.js
@@ -19,7 +19,7 @@ const getLeaderBoard = async (committee_name, month) => {
     const committee = await Committee.findOne({ where: { committee_name: committee_name } })
     const committee_id = committee.committee_id;
     console.log("bsm ellah")
-    const users = await sequelize.query(`SELECT COALESCE(sum(User_Tasks.value),0) as user_grades,COALESCE(sum(Tasks.task_value),0) as task_grades,Tasks.type, Users.user_name, Users.image from User_Tasks LEFT join Tasks on Tasks.task_id = User_Tasks.task_id RIGHT JOIN Users ON Users.user_id = User_Tasks.user_id where Tasks.type is not null and users.is_admin = false and tasks.committee_id = ${committee_id} and tasks.month = ${month} and (( users.first_com_id = ${committee_id} and first_com_active = true ) or (users.second_com_id = ${committee_id}  and second_com_active = true ) ) GROUP by Users.user_name, Tasks.type`, { type: sequelize.QueryTypes.SELECT });
+    const users = await sequelize.query(`SELECT COALESCE(sum(User_Tasks.value),0) as user_grades,COALESCE(sum(Tasks.task_value),0) as task_grades,Tasks.type, Users.user_name, Users.image from User_Tasks LEFT join Tasks on Tasks.task_id = User_Tasks.task_id RIGHT JOIN Users ON Users.user_id = User_Tasks.user_id where Tasks.type is not null and users.is_admin = false and Tasks.committee_id = ${committee_id} and Tasks.month = ${month} and (( Users.first_com_id = ${committee_id} and Users.first_com_active = true ) or (Users.second_com_id = ${committee_id}  and Users.second_com_active = true ) ) GROUP by Users.user_name, Tasks.type`, { type: sequelize.QueryTypes.SELECT });
     let new_board = []
 
     users.map((user, index) => {
